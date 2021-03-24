2972dd4815a0a14d0acc80a87d43ca8789fe67d6
Denis Slavik <denis.slavik@gmail.com>
2021-03-24 15:22:44 +0100

    Update demo app

--- demo/ReactNativeIDnowSample/App.tsx
+++ demo/ReactNativeIDnowSample/App.tsx
@@ -1,42 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- *
- * @format
- */
-
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button, Alert} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
 import {IDnowManager} from 'react-native-idnow';
 
-interface Props {}
-export default class App extends Component<Props> {
-  render() {
-    const options = {
-      showVideoOverviewCheck: true,
-      transactionToken: 'TST-QAFWM',
-      environment: 'TEST',
-    };
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>react-native-idnow demo</Text>
-        <Button
-          title="Start video identification"
-          onPress={async () => {
-            try {
-              const resp = await IDnowManager.startVideoIdent(options);
-              console.warn('==== resp', resp);
-            } catch (e) {
-              console.warn('==== e', e);
-            }
-          }}
-        />
-      </View>
-    );
-  }
+export default function App() {
+  const options = {
+    showVideoOverviewCheck: true,
+    transactionToken: 'TST-QAFWM',
+    environment: 'TEST',
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>react-native-idnow demo</Text>
+      <Button
+        title="Start video identification"
+        onPress={async () => {
+          try {
+            const resp = await IDnowManager.startVideoIdent(options);
+            console.warn('==== resp', resp);
+          } catch (e) {
+            console.warn('==== e', e);
+          }
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
